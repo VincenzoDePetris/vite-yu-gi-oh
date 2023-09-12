9880ec71d80a1820ff10d6ebd9ff87c03c37231c
VincenzoDePetris <vince02@live.it>
2023-09-12 17:07:25 +0200

    add: cards

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,34 @@
 import cardContainer from "./components/cardContainer.vue";
 
 export default {
+  data() {
+    return {
+      title: "Yu-Gi-Oh Api",
+    };
+  },
   components: { cardContainer },
 };
 </script>
 
 <template>
   <div>
+    <div class="dad">
+      <div class="title">
+        <h1>{{ title }}</h1>
+      </div>
+    </div>
     <cardContainer />
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@use "./components/general.scss" as *;
+.dad {
+  background-color: white;
+  height: 50px;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+}
+</style>
--- src/components/cardContainer.vue
+++ src/components/cardContainer.vue
@@ -6,14 +6,17 @@ export default {
     return {
       title: "Yu-Gi-Oh Api",
       cards: [],
+      number: 20,
     };
   },
   methods: {
     fetchCards() {
-      axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php").then((res) => {
-        console.log(res.data);
-        this.cards = res.data;
-      });
+      axios
+        .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
+        .then((res) => {
+          console.log(res.data.data);
+          this.cards = res.data.data;
+        });
     },
   },
   created() {
@@ -24,15 +27,53 @@ export default {
 
 <template>
   <section class="container">
-    <h1>{{ title }}</h1>
-
-    <div class="row g-4">
+    <div class="number-cards">
+      <p>Found {{ number }} cards</p>
+    </div>
+    <div class="row">
       <div class="col" v-for="card in cards">
-        {{ card.name }}
-        {{ card.type }}
+        <img :src="card.card_images[0].image_url" alt="" />
+        <p class="name">
+          {{ card.name }}
+        </p>
+        <p class="archetype">
+          {{ card.archetype }}
+        </p>
       </div>
     </div>
   </section>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.container {
+  width: 1400px;
+  margin: auto;
+  background-color: white;
+  .number-cards {
+    background-color: black;
+    color: whitesmoke;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 0;
+  }
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    .col {
+      background-color: rgb(212, 143, 56);
+      margin: 10px;
+      width: 250px;
+      text-align: center;
+      .name {
+        color: whitesmoke;
+        font-size: 1.3rem;
+      }
+      img {
+        width: 250px;
+        height: 350px;
+      }
+    }
+  }
+}
+</style>
