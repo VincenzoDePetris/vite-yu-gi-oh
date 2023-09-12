73253d9b9f89beff932f2d387e4cce77f92f8b14
VincenzoDePetris <vince02@live.it>
2023-09-12 15:52:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import cardContainer from "./components/cardContainer.vue";
+
+export default {
+  components: { cardContainer },
+};
+</script>
+
+<template>
+  <div>
+    <cardContainer />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/cardContainer.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      title: "Yu-Gi-Oh Api",
+      cards: [],
+    };
+  },
+  methods: {
+    fetchCards() {
+      axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php").then((res) => {
+        console.log(res.data);
+        this.cards = res.data;
+      });
+    },
+  },
+  created() {
+    this.fetchCards();
+  },
+};
+</script>
+
+<template>
+  <section class="container">
+    <h1>{{ title }}</h1>
+
+    <div class="row g-4">
+      <div class="col" v-for="card in cards">
+        {{ card.name }}
+        {{ card.type }}
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
